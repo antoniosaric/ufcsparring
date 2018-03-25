05e4b66b6761eda0250b2db3435af1bbc30b0319
Tony <saric.tony@gmail.com>
2018-03-25 15:57:19 -0700

    adds inintial files to ufc

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+
+  def index 
+    @users = User.all.order("created_at DESC")
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+end
